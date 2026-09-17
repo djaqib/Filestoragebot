a103fab446b9a7a3433be80629f0d7083b16d17a
djaqib <42904416+djaqib@users.noreply.github.com>
2026-09-16 18:09:49 -0700

    Fix import statement for RealDictCursor

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from psycopg2.extras 
+import RealDictCursor
 
 def get_db_connection():
     # Uses standard Postgres connection string (e.g., from Neon console)
